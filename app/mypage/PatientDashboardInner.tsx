a79bba6e330498c0e5098f9b04fa063e4f312635
Jin MItsui <administer@JinnoMacBook-Pro.local>
2025-12-08 13:26:29 +0900

    update

--- app/mypage/PatientDashboardInner.tsx
+++ app/mypage/PatientDashboardInner.tsx
@@ -959,8 +959,8 @@ setData(finalData);
           <p className="mt-2 text-[11px] text-slate-500">
             ※ 診察中・夜間など、返信までお時間をいただく場合があります。
           </p>
-        </section>
-      </main>
-    </div>
-  );
+    </section>
+  </main>
+</div>
+);
 }
