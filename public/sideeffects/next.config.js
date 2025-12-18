57ad71622fc4c42feafd6ff8de8e1b87e10af854
Jin MItsui <administer@JinnoMacBook-Pro.local>
2025-12-18 11:52:19 +0900

    Remove unused /register/phone page and add redirect to /mypage/init

--- public/sideeffects/next.config.js
+++ public/sideeffects/next.config.js
@@ -12,3 +12,18 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/register/phone",
+        destination: "/mypage/init",
+        permanent: true, // 308
+      },
+    ];
+  },
+};
+
+export default nextConfig;
